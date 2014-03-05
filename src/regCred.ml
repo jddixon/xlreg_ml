566b09ab9d0d0eb0e564f0d270895be05b9e4312
Jim Dixon <jddixon@gmail.com>
2014-03-05 14:28:04 -0800

    initial regCred

---
+++ src/regCred.ml
@@ -0,0 +1,15 @@
+(* regCred.t *)
+
+(* endPoints must be an array *)
+
+type nodeID = { bytes : string }    (* should be uint8 array *)
+
+type endPoint = { protocol : string; address: int ;   (* must be uint32 *)
+    port: int; }    (* should be uint16 *)
+
+type rsaPublicKey = { e : int; n : int; }   (* MUST be bigint *)
+
+type decimalVersion = {version : int ; }    (* must be uint32 *)
+
+type regCred = {name : string; id : nodeID; ck : rsaPublicKey; 
+    sk : rsaPublicKey; endPoints : endPoint array;  version: decimalVersion }
